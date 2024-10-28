3ebba425f10e5f8c0383bca0525b19f3e965376a
Anus Shafiq <sheikhanus925@gmail.com>
2024-10-29 00:39:32 +0500

    update card & acoordion sections

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var password =
   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+<>~?/";
 
 var passLenght = 15;
-var generatePass = "";
 
-for (var i = 0; i < passLenght; i++) {
-  var passRandomIndex = Math.floor(Math.random() * password.length);
-  generatePass += password[passRandomIndex];
+function generator() {
+  var generatePass = "";
+  for (var i = 0; i < passLenght; i++) {
+    var passRandomIndex = Math.floor(Math.random() * password.length);
+    generatePass += password[passRandomIndex];
+  }
+  document.getElementById("input").innerHTML = `<span>${generatePass}</span>`;
 }
 
 // document.write(`Random Password is: ${generatePass}`);
