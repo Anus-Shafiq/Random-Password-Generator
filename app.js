475aeb765edd427de7d5a9c4895bb06a061ba505
Anus Shafiq <sheikhanus925@gmail.com>
2025-01-15 23:04:03 +0500

    update code with conditions

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
-var password =
-  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+<>~?/";
+let uppercase = document.getElementById("uppercase");
+let lowercase = document.getElementById("lowercase");
+let number = document.getElementById("num");
+let symbol = document.getElementById("symbol");
 
-var passLenght = 15;
+let range = document.getElementById("range");
+let rangeNum = document.getElementById("rangeNum");
+var passLenght = "";
+var uppercaseEng = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let lowercaseEng = "abcdefghijklmnopqrstuvwxyz";
+let numbers = "1234567890";
+let symbols = "!@#$%^&*()_+<>~?/";
 
-function generator() {
-  var generatePass = "";
-  for (var i = 0; i < passLenght; i++) {
-    var passRandomIndex = Math.floor(Math.random() * password.length);
-    generatePass += password[passRandomIndex];
-  }
-  document.getElementById("input").innerHTML = `<span>${generatePass}</span>`;
+function num() {
+  rangeNum.innerText = range.value;
+  passLenght = range.value;
+
+  generator();
 }
 
-// document.write(`Random Password is: ${generatePass}`);
+window.onload = num;
+function generator() {
+  let char = "";
+  if (uppercase.checked) {
+    char += uppercaseEng;
+  }
+  if (lowercase.checked) {
+    char += lowercaseEng;
+  }
+  if (number.checked) {
+    char += numbers;
+  }
+  if (symbol.checked) {
+    char += symbols;
+  }
+  if (char === "") {
+    alert("Please select at least any one character");
+    return;
+  }
+
+  var generatePass = "";
+  for (var i = 0; i < passLenght; i++) {
+    var passRandomIndex = Math.floor(Math.random() * char.length);
+    generatePass += char[passRandomIndex];
+
+    document.getElementById("input").innerHTML = `<span>${generatePass}</span>`;
+  }
+}
