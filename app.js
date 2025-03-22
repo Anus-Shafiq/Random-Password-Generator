c6bb3c046d223d2bac2f674799caa8330e398341
Anus Shafiq <sheikhanus925@gmail.com>
2025-03-22 17:07:18 +0500

    Add copy button & other features

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let uppercase = document.getElementById("uppercase");
 let lowercase = document.getElementById("lowercase");
 let number = document.getElementById("num");
 let symbol = document.getElementById("symbol");
-
+let label = document.getElementById("label");
 let range = document.getElementById("range");
 let rangeNum = document.getElementById("rangeNum");
 var passLenght = "";
@@ -10,10 +10,13 @@ var uppercaseEng = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 let lowercaseEng = "abcdefghijklmnopqrstuvwxyz";
 let numbers = "1234567890";
 let symbols = "!@#$%^&*()_+<>~?/";
+let pass = document.getElementById("input");
+let copyBtn = document.getElementById("copyBtn");
 
 function num() {
   rangeNum.innerText = range.value;
   passLenght = range.value;
+  copyBtn.innerText = "Copy";
 
   generator();
 }
@@ -43,6 +46,59 @@ function generator() {
     var passRandomIndex = Math.floor(Math.random() * char.length);
     generatePass += char[passRandomIndex];
 
-    document.getElementById("input").innerHTML = `<span>${generatePass}</span>`;
+    pass.value = generatePass;
+  }
+
+  if (pass.value.length < 6) {
+    label.classList.remove(
+      "bg-success-subtle",
+      "bg-danger-subtle",
+      "bg-warning",
+      "bg-success"
+    );
+
+    label.classList.add("bg-danger");
+    label.innerText = "Very weak";
+  } else if (pass.value.length < 8) {
+    label.classList.remove(
+      "bg-danger",
+      "bg-success-subtle",
+      "bg-warning",
+      "bg-success"
+    );
+    label.classList.add("bg-danger-subtle");
+    label.innerText = "Weak";
+  } else if (pass.value.length < 10) {
+    label.classList.remove(
+      "bg-danger",
+      "bg-danger-subtle",
+      "bg-success-subtle",
+      "bg-success"
+    );
+    label.classList.add("bg-warning");
+    label.innerText = "Good";
+  } else if (pass.value.length < 12) {
+    label.classList.remove(
+      "bg-danger",
+      "bg-danger-subtle",
+      "bg-success",
+      "bg-warning"
+    );
+    label.classList.add("bg-success-subtle");
+    label.innerText = "Strong";
+  } else if (pass.value.length >= 12) {
+    label.classList.remove(
+      "bg-danger",
+      "bg-danger-subtle",
+      "bg-warning",
+      "bg-success-subtle"
+    );
+    label.classList.add("bg-success");
+    label.innerText = "Very Strong";
   }
 }
+
+copyBtn.addEventListener("click", () => {
+  navigator.clipboard.writeText(pass.value);
+  copyBtn.innerText = "Copied!";
+});
